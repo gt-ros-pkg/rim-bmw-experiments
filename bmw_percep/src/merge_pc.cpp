1a187617fb9db7f00a28f63929f63dcf6de2e6a9
Shray Bansal <shray.bansal@gmail.com>
2014-09-19 18:04:18 -0400

    Merge PC synchronizes the clouds it publishes.

--- bmw_percep/src/merge_pc.cpp
+++ bmw_percep/src/merge_pc.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <pcl/point_cloud.h>
+#include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/openni_grabber.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -17,8 +17,10 @@
 #include <pcl_ros/point_cloud.h>
 #include <tf/transform_listener.h>
 #include <tf_conversions/tf_eigen.h>
-
-
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <pcl_conversions/pcl_conversions.h>
 /**
    Subscribe to two point clouds, transform into 
    one frame. Merge them and then publish as a
@@ -26,6 +28,7 @@
 **/
 
 typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudSM;
+typedef sensor_msgs::PointCloud2 PCMsg;
 
 struct pclTransform{
   Eigen::Vector3d translation;
@@ -42,57 +45,148 @@ void back_call(const PointCloudSM::ConstPtr&);
 void voxelize_cloud(PointCloudSM::Ptr cloud, 
 		    PointCloudSM::Ptr filtered_cloud, 
 		    float leaf_size=0.06);
+void callback(const PCMsg::ConstPtr& front_pc, 
+	      const PCMsg::ConstPtr& back_pc );
+
+void get_transform_fb( string back_frame, string front_frame, 
+		       pclTransform &back_transform, pclTransform &front_transform);
+
 //GLOBALs
 //TODO: Get Rid of 'em
+//Transformers for the PointClouds
+pclTransform back_transform, front_transform;
+bool first_frame;
 string front_frame, back_frame;
-PointCloudSM::Ptr front_pc;
-PointCloudSM::Ptr back_pc;
 bool new_pc_f, new_pc_b;
+ros::Publisher pc_pub;
+string new_frame;
+PointCloudSM::Ptr back_pc, new_b_pc, pub_pc, front_pc;
 
 //TODO: Time stamp checks on the two pointclouds
 int main(int argc, char** argv)
 {
+  first_frame=true;
   //initialize shared pointers
   front_pc = boost::shared_ptr<PointCloudSM>(new PointCloudSM);
   back_pc = boost::shared_ptr<PointCloudSM>(new PointCloudSM);
   
+  new_b_pc = boost::shared_ptr<PointCloudSM>(new PointCloudSM);
+  pub_pc = boost::shared_ptr<PointCloudSM>(new PointCloudSM);
+
+
   //ros
+  
+  string bg_back_topic = "background_sub_back/pc1_out";
+  string bg_front_topic = "background_sub_front/pc1_out";
+  
   ros::init(argc, argv, "merge_pc");
   ros::NodeHandle nh;
-  ros::Publisher pc_pub = nh.advertise<PointCloudSM> 
+  pc_pub = nh.advertise<PointCloudSM> 
     ("/kinect_both/depth_registered/points", 1);
-  ros::Subscriber front_sub = nh.subscribe<PointCloudSM> 
-    ("/kinect_front/depth_registered/points", 1, front_call);
-  ros::Subscriber back_sub = nh.subscribe<PointCloudSM> 
-    ("/kinect_back/depth_registered/points", 1, back_call);
+  // ros::Subscriber front_sub = nh.subscribe<PointCloudSM> 
+  //   ("/kinect_front/depth_registered/points", 1, front_call);
+  // ros::Subscriber back_sub = nh.subscribe<PointCloudSM> 
+  //   ("/kinect_back/depth_registered/points", 1, back_call);
+
+message_filters::Subscriber<PCMsg> 
+  front_sub(nh, bg_front_topic, 1);
+message_filters::Subscriber<PCMsg> 
+    back_sub(nh, bg_back_topic, 1);
+
+typedef message_filters::sync_policies::ApproximateTime<PCMsg, PCMsg> MySyncPolicy;
+
+message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(4), back_sub, front_sub);
+
+  sync.registerCallback(boost::bind(&callback, _1, _2));
 
   //world frame for easy clipping of workspace
-  string new_frame = "table_link";
+  new_frame = "table_link";
 
   //Point Clouds
   PointCloudSM::Ptr pub_pc(new PointCloudSM);
   PointCloudSM::Ptr new_b_pc(new PointCloudSM), new_f_pc(new PointCloudSM);
 
+
+  boost::timer timer_transform, timer_concat, timer_total;
+  double time_transform=0.0, time_concat=0.0, time_total=0.0;
+  unsigned long n_frames=0;
+
+
+  ros::spin();
+
+  return 0;
+}
+
+void callback(const PCMsg::ConstPtr& front_pc_, const PCMsg::ConstPtr& back_pc_ )
+{
+  
+  pcl::PCLPointCloud2 back_pc1;
+
+  pcl_conversions::toPCL(*back_pc_, back_pc1);
+  pcl::fromPCLPointCloud2(back_pc1, *back_pc);
+
+  pcl::PCLPointCloud2 front_pc1;
+  pcl_conversions::toPCL(*front_pc_, front_pc1);
+  pcl::fromPCLPointCloud2(front_pc1, *front_pc);
+
+  back_frame = front_pc->header.frame_id;
+ 
+  front_frame = back_pc->header.frame_id;
+  
+
+  if(first_frame){
+    get_transform_fb(back_frame, front_frame, back_transform, front_transform);
+    first_frame=false;
+  }
+
+  //transform points
+  pcl::transformPointCloud(*back_pc, *new_b_pc, front_transform.translation, 
+      			       front_transform.rotation);
+  pcl::transformPointCloud(*front_pc, *pub_pc, back_transform.translation, 
+  			   back_transform.rotation);
+
+
+  *pub_pc += *new_b_pc;
+  
+  pub_pc->header.frame_id = new_frame;
+  pc_pub.publish(*pub_pc);
+
+}
+
+ void voxelize_cloud(PointCloudSM::Ptr cloud, 
+		     PointCloudSM::Ptr filtered_cloud, 
+		    float leaf_size/*=0.01*/)
+{
+  float voxel_size=leaf_size;
+
+  //Voxelize the space
+  pcl::VoxelGrid<pcl::PointXYZRGB> vg;
+  //PointCloudSM::Ptr cloud_filtered(new PointCloudSM);
+  vg.setInputCloud(cloud);
+  vg.setLeafSize(voxel_size, voxel_size, voxel_size);
+  vg.filter(*filtered_cloud);
+    
+  // //debug
+  // cout << "PointCloud after filtering has: " << 
+  //   filtered_cloud->points.size ()  << " data points." << endl;
+  // cout << "Vs. " << cloud->points.size() << " before." << endl;
+
+}
+
+void get_transform_fb( string back_frame, string front_frame, 
+		       pclTransform &back_transform, pclTransform &front_transform)
+{
   //Listeners for both transformations
   tf::TransformListener trans_back_table;
   tf::TransformListener trans_front_table;
 
-  //Transformers for the PointClouds
-  pclTransform back_transform, front_transform;
 
   //listen for transform until one is gotten
   //since its static, don't look for transform afterwards
   bool found_back_t=false, found_front_t=false;
 
-  new_pc_f = false;
-  new_pc_b = false;
-
-  while (nh.ok() && !(found_back_t && found_front_t)){
-    ros::spinOnce();
+  while (!(found_back_t && found_front_t)){
     
-    if (!(new_pc_f && new_pc_b))
-      continue; // spin again if both Pointclouds not get
-
     //Back kinect transform
     if(!found_back_t){
       tf::StampedTransform t_back;
@@ -142,8 +236,6 @@ int main(int argc, char** argv)
     
   }
 
-  new_pc_f = false;
-  new_pc_b = false;
   //pcl::transformPointCloud(front_pc, pub_pc, TransMat ); 
 
   //debug
@@ -157,115 +249,6 @@ int main(int argc, char** argv)
   shr_cv_utils::to_trans_mat(front_transform.rotation, front_transform.translation,
 			     front_trans_mat);
 
-  boost::timer timer_transform, timer_concat, timer_total;
-  double time_transform=0.0, time_concat=0.0, time_total=0.0;
-  unsigned long n_frames=0;
-
-  while(ros::ok()){
-
-    timer_total.restart();
-
-    ros::spinOnce();
-
-    //if both PCs new then concatenate
-    if (new_pc_f && new_pc_b){
-      
-      timer_transform.restart();
-
-      // pcl::transformPointCloud(*back_pc, *new_b_pc, back_transform.translation, 
-      // 			       back_transform.rotation);
-      // pcl::transformPointCloud(*front_pc, *pub_pc, front_transform.translation, 
-      // 			       front_transform.rotation);
-
-      // cout << "Get to voxelize??" << endl;
-      //Voxelize-first
-      voxelize_cloud(back_pc, new_b_pc);
-      voxelize_cloud(front_pc, new_f_pc);
-      
-      // pcl::copyPointCloud(*back_pc, *new_b_pc);
-      // pcl::copyPointCloud(*front_pc, *new_f_pc);
-
-      pcl::transformPointCloud(*new_b_pc, *back_pc, back_transform.translation, 
-      			       back_transform.rotation);
-      pcl::transformPointCloud(*new_f_pc, *pub_pc, front_transform.translation, 
-      			       front_transform.rotation);
-
-      // shr_cv_utils::transPoints(back_pc, back_trans_mat, new_b_pc);
-      // shr_cv_utils::transPoints(front_pc, front_trans_mat, pub_pc);
-
-      time_transform+=timer_transform.elapsed();
-
-      //concatenate
-      // pcl::transformPointCloud(front_pc, pub_pc, TransMat ); 
-      timer_concat.restart();
-      *pub_pc += *back_pc;
-      time_concat += timer_concat.elapsed();
-
-      //set frame
-      pub_pc->header.frame_id = new_frame;
-
-      new_pc_f = false;
-      new_pc_b = false;
-
-      //publish
-      pc_pub.publish(pub_pc);
-
-      time_total+=timer_total.elapsed();
-      n_frames++;
-
-      //debug
-      if (n_frames%10 == 0)
-      	{
-      	  cout << "\n**********COMPUTE-TIMES**********\n";
-      	  cout << "Transform = " << time_transform/n_frames << " seconds\n";
-      	  cout << "Concatenation = " << time_concat/n_frames << " seconds\n";
-      	  cout << "Total = " << time_total/n_frames << " seconds" << endl;
-
-      	}
-	
-    }
-
-  }
-
-  return 0;
-}
-
-// callback:
-void front_call(const PointCloudSM::ConstPtr& cloud)
-{
-  if (!new_pc_f){
-    front_frame = cloud->header.frame_id;
-    *front_pc = *cloud;
-    new_pc_f = true;
-  }
-    
-}
-
-void back_call(const PointCloudSM::ConstPtr& cloud)
-{
-  if (!new_pc_b){
-    back_frame = cloud->header.frame_id;
-    *back_pc = *cloud;
-    new_pc_b = true;
-  }
-}
-
- void voxelize_cloud(PointCloudSM::Ptr cloud, 
-		     PointCloudSM::Ptr filtered_cloud, 
-		    float leaf_size/*=0.01*/)
-{
-  float voxel_size=leaf_size;
-
-  //Voxelize the space
-  pcl::VoxelGrid<pcl::PointXYZRGB> vg;
-  //PointCloudSM::Ptr cloud_filtered(new PointCloudSM);
-  vg.setInputCloud(cloud);
-  vg.setLeafSize(voxel_size, voxel_size, voxel_size);
-  vg.filter(*filtered_cloud);
-    
-  // //debug
-  // cout << "PointCloud after filtering has: " << 
-  //   filtered_cloud->points.size ()  << " data points." << endl;
-  // cout << "Vs. " << cloud->points.size() << " before." << endl;
+  return;
 
 }
